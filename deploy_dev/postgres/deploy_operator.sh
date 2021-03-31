3fed995e3fdf95c3225cc82c5d886c3df05152e8
Bakhtiarov Sergey <bakhtiarov.sergey@gmail.com>
2021-03-31 14:20:42 +0900

    fix: zalando operator version 1.6.0 freeze

--- deploy_dev/postgres/deploy_operator.sh
+++ deploy_dev/postgres/deploy_operator.sh
@@ -4,9 +4,12 @@
 # kubectl apply -f operator-postgres-manifest.yaml 
 # The postgresql "acid-postgres-cluster" is invalid: spec.postgresql.version: Unsupported value: "13": supported values: "9.3", "9.4", "9.5", "9.6", "10", "11", "12"
 
-git clone https://github.com/zalando/postgres-operator.git
+# git clone https://github.com/zalando/postgres-operator.git
+wget https://github.com/zalando/postgres-operator/archive/refs/tags/v1.6.0.zip
+unzip v1.6.0.zip
+mv postgres-operator-1.6.0 postgres-operator
 helm install postgres-operator postgres-operator/charts/postgres-operator
 echo "Waiting postgres operator for 60 seconds..."
 sleep 60
 kubectl apply -f postgres-manifest.yaml
-rm -rf postgres-operator
+rm -rf postgres-operator v1.6.0.zip
