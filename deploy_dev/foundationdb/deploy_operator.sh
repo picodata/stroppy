8d5d9e92175d470c9c0738a0f6b948eb92d3e963
Bakhtiarov Sergey <bakhtiarov.sergey@gmail.com>
2021-04-09 14:11:15 +0900

    fix: fdb operator uses rancher local storage, operator version 0.27.1 >> 0.31.1

--- deploy_dev/foundationdb/deploy_operator.sh
+++ deploy_dev/foundationdb/deploy_operator.sh
@@ -1,8 +1,8 @@
 minikube addons enable metallb
-kubectl apply -f https://raw.githubusercontent.com/FoundationDB/fdb-kubernetes-operator/v0.27.1/config/crd/bases/apps.foundationdb.org_foundationdbclusters.yaml
-kubectl apply -f https://raw.githubusercontent.com/FoundationDB/fdb-kubernetes-operator/v0.27.1/config/crd/bases/apps.foundationdb.org_foundationdbbackups.yaml
-kubectl apply -f https://raw.githubusercontent.com/FoundationDB/fdb-kubernetes-operator/v0.27.1/config/crd/bases/apps.foundationdb.org_foundationdbrestores.yaml
-kubectl apply -f https://raw.githubusercontent.com/foundationdb/fdb-kubernetes-operator/v0.27.1/config/samples/deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/FoundationDB/fdb-kubernetes-operator/v0.31.1/config/crd/bases/apps.foundationdb.org_foundationdbclusters.yaml
+kubectl apply -f https://raw.githubusercontent.com/FoundationDB/fdb-kubernetes-operator/v0.31.1/config/crd/bases/apps.foundationdb.org_foundationdbbackups.yaml
+kubectl apply -f https://raw.githubusercontent.com/FoundationDB/fdb-kubernetes-operator/v0.31.1/config/crd/bases/apps.foundationdb.org_foundationdbrestores.yaml
+kubectl apply -f https://raw.githubusercontent.com/foundationdb/fdb-kubernetes-operator/v0.31.1/config/samples/deployment.yaml
 echo "Waiting foundationdb deploy for 60 seconds..."
 sleep 60
 kubectl apply -f cluster_with_client.yaml
