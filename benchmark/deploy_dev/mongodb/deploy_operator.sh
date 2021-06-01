f4099065eed83dafd796f8e14f12fc518a133469
Alexander Andreev <a.hypostasys@gmail.com>
2021-06-01 09:49:04 +0300

    moveout_deploy_dev: папки benchmark/deploy_dev и benchmark/yugabyte вынесены в отдельный репозиторий deploy-references

--- benchmark/deploy_dev/mongodb/deploy_operator.sh
+++
@@ -1,9 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/mongodb/mongodb-kubernetes-operator/master/config/crd/bases/mongodbcommunity.mongodb.com_mongodbcommunity.yaml
-kubectl apply -f https://raw.githubusercontent.com/mongodb/mongodb-kubernetes-operator/master/config/rbac/role.yaml
-kubectl apply -f https://raw.githubusercontent.com/mongodb/mongodb-kubernetes-operator/master/config/rbac/role_binding.yaml
-kubectl apply -f https://raw.githubusercontent.com/mongodb/mongodb-kubernetes-operator/master/config/rbac/service_account.yaml
-kubectl apply -f https://raw.githubusercontent.com/mongodb/mongodb-kubernetes-operator/master/config/manager/manager.yaml
-echo "Waiting mongodb deploy for 60 seconds..."
-sleep 60
-kubectl apply -f mongodb-cluster.yaml
-kubectl apply -f mongodb-client.yaml
--- benchmark/deploy_dev/monitoring/deploy_operator.sh
+++
@@ -1,8 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install grafana-stack prometheus-community/kube-prometheus-stack
-minikube addons enable ingress
-kubectl apply -f metrics-server.yaml
-echo "Waiting grafana deploy for 60 seconds..."
-sleep 60
-kubectl apply -f ingress-grafana.yaml
