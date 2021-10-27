7fc1613485428d96fb85fb12a2e5d0dec5bd7319
Alexander Andreev <a.andreev@picodata.io>
2021-10-27 10:12:15 +0300

    fix: добавил проброс параметра с типом провайдера на под строппи при запуске тестов.

--- cmd/stroppy/commands/pop.go
+++ cmd/stroppy/commands/pop.go
@@ -11,6 +11,7 @@ import (
 	"github.com/spf13/cobra"
 	"gitlab.com/picodata/stroppy/internal/deployment"
 	"gitlab.com/picodata/stroppy/pkg/database/config"
+	"gitlab.com/picodata/stroppy/pkg/engine/provider"
 	"gopkg.in/inf.v0"
 )
 
