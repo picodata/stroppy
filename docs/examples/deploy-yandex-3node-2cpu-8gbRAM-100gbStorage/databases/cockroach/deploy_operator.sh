351b23a24f5f8bb6e1820a08b1ca832c9c888d0f
Alexander Andreev <a.andreev@picodata.io>
2021-11-24 09:32:58 +0300

    fix: поправлены замечания ревью

--- docs/examples/deploy-yandex-3node-2cpu-8gbRAM-100gbStorage/databases/cockroach/deploy_operator.sh
+++ docs/examples/deploy-yandex-3node-2cpu-8gbRAM-100gbStorage/databases/cockroach/deploy_operator.sh
@@ -5,25 +5,27 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "$SCRIPT_DIR/../../common.sh"
 
 
+errorless_run "creating role bindings" \
+kubectl create clusterrolebinding ubuntu-cluster-admin-binding --clusterrole=cluster-admin
+
 run "applying crd for the operator" \
-kubectl apply -f "$SCRIPT_DIR/deploy_config/crd/bases/crdb.cockroachlabs.com_crdbclusters.yaml"
+kubectl apply -f "$SCRIPT_DIR/operator/crds.yaml"
+
+sleep 10
 
 run "installing operator" \
-kubectl apply -f "$SCRIPT_DIR/install/operator.yaml"
+kubectl apply -f "$SCRIPT_DIR/operator/operator.yaml"
+
+sleep 10
 
 
 # ==============
-run "creating client operator" kubectl create -f "$SCRIPT_DIR/client-operator.yaml"
-
 run "instantiating cocroachdb" kubectl apply -f "$SCRIPT_DIR/crdb.yaml"
 
-
-# ==============
-run "applying rbac rules" \
-kubectl apply -f "$SCRIPT_DIR/deploy_config/rbac/database.yaml"
+run "creating client" kubectl create -f "$SCRIPT_DIR/client-operator.yaml"
 
 
-# === monitoring ====
+# == monitoring ==============
 run "labeling cockroachdb svc for prometheus" \
 kubectl label svc cockroachdb prometheus=cockroachdb
 
